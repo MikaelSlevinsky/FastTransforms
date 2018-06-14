40da4e830f059c055cbaa447a84c0684c2bd433f
dawese <40175309+dawese@users.noreply.github.com>
2018-06-14 15:39:12 -0500

    Add files via upload
    Adding swap and permute_AVX to develop AVX capability.

--- src/permute.c
+++ src/permute.c
@@ -10,6 +10,7 @@ void local_transpose_SSE(const double * A, double * B, const int N, const int M)
             B[(2*i)%(2*N)+(2*i)/(2*N)+j*N] = A[i+j*N];
 }
 
+
 void local_reverse_transpose_SSE(double * A, const double * B, const int N, const int M) {
     for (int i = 0; i < N; i++)
         A[i] = B[i];
@@ -46,6 +47,25 @@ void local_odd_reverse_transpose_SSE(double * C, const double * E, const int N, 
             C[i+j*N] = E[(2*i)%(2*N)+(2*i)/(2*N)+j*N];
 }
 
+// Not sure why he skips the first column in his permutes. 
+// Breaks compatibility with even number of columns.
+// I'll leave it in but comment it out. Set j = 1. 
+
+void permute_AVX(const double * A, double * B, const int N, const int M) {
+    //for (int i = 0; i < N; i++)
+    //    B[i] = A[i];
+    for (int j = 0; j < M; j += 4)
+        for (int i = 0; i < 4*N; i++)
+            B[(4*i)%(4*N)+(4*i)/(4*N)+j*N] = A[i+j*N];
+}
+
+void permute_t_AVX(double * A, const double * B, const int N, const int M) {
+    //for (int i = 0; i < N; i++)
+    //    B[i] = A[i];
+    for (int j = 0; j < M; j += 4)
+        for (int i = 0; i < 4*N; i++)
+            A[i+j*N] = B[(4*i)%(4*N)+(4*i)/(4*N)+j*N];
+}
 
 void permute_tri_SSE(const double * A, double * B, const int N, const int M) {
     for (int j = 0; j < M; j += 2)
@@ -69,4 +89,55 @@ void permute_t_tri_AVX(double * A, const double * B, const int N, const int M) {
     for (int j = 0; j < M; j += 4)
         for (int i = 0; i < 4*N; i++)
             A[i+j*N] = B[(4*i)%(4*N)+(4*i)/(4*N)+j*N];
+}
+
+void swap(double * A, double * B, const int M){
+    double tmp = 0.0;
+    for(int i = 0; i < 2*M; i++){
+
+        tmp = A[i];
+        A[i] = B[i];
+        B[i] = tmp;
+    }
+}
+
+void swap_SSE(double * A, double * B, const int M){
+    __m128d *A_SSE = (__m128d*) A;
+    __m128d *B_SSE = (__m128d*) B;
+    __m128d tmp = _mm_set1_pd(0.0);
+    for(int i = 0; i < M; i++){
+        
+        tmp = A_SSE[i];
+        vstore2((A+i*2), B_SSE[i]);
+        vstore2((B+i*2), tmp);
+    }
+}
+
+// Note : Call gcc with -mavx flag for this feature
+void swap_AVX(double * A, double * B, const int M){
+    __m256d *A_AVX = (__m256d*) A;
+    __m256d *B_AVX = (__m256d*) B;
+    __m256d tmp = _mm256_set1_pd(0.0);
+    for(int i = 0; i < M/2; i++){
+        
+        tmp = A_AVX[i];
+        vstore4((A+i*4), B_AVX[i]);
+        vstore4((B+i*4), tmp);
+    }
+}
+
+void two_warp(const int M, const int N, double * A){
+    for (int i = 0; i < N; i+=8){
+        swap_AVX(A+(2+i)*M, A+(4+i)*M, M);     
+    }
+}
+
+void four_warp(const int M, const int N, double * A){
+    for (int i = 0; i < N; i+=16){
+            
+        swap_AVX(A+(i+2)*M, A+(i+4)*M, M);
+        swap_AVX(A+(i+4)*M, A+(i+8)*M, M);
+        swap_AVX(A+(i+6)*M, A+(i+12)*M, M);
+        swap_AVX(A+(i+10)*M, A+(i+12)*M, M);
+    }
 }
