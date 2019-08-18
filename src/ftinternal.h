bcf78a560883ae53ef88f547752d588aa1499bcb
Mikael Slevinsky <Richard.Slevinsky@umanitoba.ca>
2019-08-18 14:16:20 -0500

    stash associated & Konoplev transforms

--- src/ftinternal.h
+++ src/ftinternal.h
@@ -132,8 +132,6 @@ double * plan_chebyshev_to_legendre(const int normcheb, const int normleg, const
 double * plan_ultraspherical_to_ultraspherical(const int norm1, const int norm2, const int n, const double lambda, const double mu);
 double * plan_jacobi_to_jacobi(const int norm1, const int norm2, const int n, const double alpha, const double beta, const double gamma, const double delta);
 double * plan_laguerre_to_laguerre(const int norm1, const int norm2, const int n, const double alpha, const double beta);
-double * plan_associated_jacobi_to_jacobi(const int norm2, const int n, const int c, const double alpha, const double beta, const double gamma, const double delta);
-double * plan_konoplev_to_jacobi(const int n, const double alpha, const double beta);
 double * plan_jacobi_to_ultraspherical(const int normjac, const int normultra, const int n, const double alpha, const double beta, const double lambda);
 double * plan_ultraspherical_to_jacobi(const int normultra, const int normjac, const int n, const double lambda, const double alpha, const double beta);
 double * plan_jacobi_to_chebyshev(const int normjac, const int normcheb, const int n, const double alpha, const double beta);
