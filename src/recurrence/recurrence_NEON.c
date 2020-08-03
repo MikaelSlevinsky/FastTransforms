b2ef0e4b508fc0b52bb91a9f4381a8109be71007
Mikael Slevinsky <Richard.Slevinsky@umanitoba.ca>
2020-08-02 23:12:32 -0500

    add chebyshev clenshaw

--- src/recurrence/recurrence_NEON.c
+++ src/recurrence/recurrence_NEON.c
@@ -7,17 +7,15 @@
     void horner_NEONf(const int n, const float * c, const int incc, const int m, float * x, float * f) {
         HORNER_KERNEL(float, float32x4_t, 4, 16, vld1q_f32, vst1q_f32, vmuladd, vall_f32)
     }
-    void clenshaw_NEON(const int n, const double * c, const int incc, const int m, double * x, double * f) {clenshaw_default(n, c, incc, m, x, f);}
-    void clenshaw_NEONf(const int n, const float * c, const int incc, const int m, float * x, float * f) {clenshaw_defaultf(n, c, incc, m, x, f);}
-    void orthogonal_polynomial_clenshaw_NEON(const int n, const double * c, const int incc, const double * A, const double * B, const double * C, const int m, double * x, double * phi0, double * f) {orthogonal_polynomial_clenshaw_default(n, c, incc, A, B, C, m, x, phi0, f);}
-    void orthogonal_polynomial_clenshaw_NEONf(const int n, const float * c, const int incc, const float * A, const float * B, const float * C, const int m, float * x, float * phi0, float * f) {orthogonal_polynomial_clenshaw_defaultf(n, c, incc, A, B, C, m, x, phi0, f);}
-    /*
     void clenshaw_NEON(const int n, const double * c, const int incc, const int m, double * x, double * f) {
         CLENSHAW_KERNEL(double, float64x2_t, 2, 8, vld1q_f64, vst1q_f64, vfmaq_f64)
     }
     void clenshaw_NEONf(const int n, const float * c, const int incc, const int m, float * x, float * f) {
         CLENSHAW_KERNEL(float, float32x4_t, 4, 8, vld1q_f32, vst1q_f32, vfmaq_f32)
     }
+    void orthogonal_polynomial_clenshaw_NEON(const int n, const double * c, const int incc, const double * A, const double * B, const double * C, const int m, double * x, double * phi0, double * f) {orthogonal_polynomial_clenshaw_default(n, c, incc, A, B, C, m, x, phi0, f);}
+    void orthogonal_polynomial_clenshaw_NEONf(const int n, const float * c, const int incc, const float * A, const float * B, const float * C, const int m, float * x, float * phi0, float * f) {orthogonal_polynomial_clenshaw_defaultf(n, c, incc, A, B, C, m, x, phi0, f);}
+    /*
     void orthogonal_polynomial_clenshaw_NEON(const int n, const double * c, const int incc, const double * A, const double * B, const double * C, const int m, double * x, double * phi0, double * f) {
         ORTHOGONAL_POLYNOMIAL_CLENSHAW_KERNEL(double, float64x2_t, 2, 8, vld1q_f64, vst1q_f64, vfmaq_f64, vfmsq_f64, vall_f64)
     }
