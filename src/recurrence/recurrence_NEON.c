50081aa56910feecfd05b6a99181840690a82d78
Mikael Slevinsky <Richard.Slevinsky@umanitoba.ca>
2020-08-03 01:16:39 -0500

    more tricks

--- src/recurrence/recurrence_NEON.c
+++ src/recurrence/recurrence_NEON.c
@@ -2,10 +2,10 @@
 
 #ifdef __aarch64__
     void horner_NEON(const int n, const double * c, const int incc, const int m, double * x, double * f) {
-        HORNER_KERNEL(double, float64x2_t, 2, 16, vld1q_f64, vst1q_f64, vmuladd, vall_f64)
+        HORNER_KERNEL(double, float64x2_t, 2, 16, vld1q_f64, vst1q_f64, vmlaq_f64, vall_f64)
     }
     void horner_NEONf(const int n, const float * c, const int incc, const int m, float * x, float * f) {
-        HORNER_KERNEL(float, float32x4_t, 4, 16, vld1q_f32, vst1q_f32, vmuladd, vall_f32)
+        HORNER_KERNEL(float, float32x4_t, 4, 16, vld1q_f32, vst1q_f32, vmlaq_f32, vall_f32)
     }
     void clenshaw_NEON(const int n, const double * c, const int incc, const int m, double * x, double * f) {
         CLENSHAW_KERNEL(double, float64x2_t, 2, 8, vld1q_f64, vst1q_f64, vmuladd, vall_f64)
