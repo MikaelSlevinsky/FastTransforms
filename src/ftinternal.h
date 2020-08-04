39af8f9c4c8943f4cc96256a42638a42e1f7104d
Mikael Slevinsky <Richard.Slevinsky@umanitoba.ca>
2020-08-03 23:35:31 -0500

    vfmaq_f** has permuted arguments

--- src/ftinternal.h
+++ src/ftinternal.h
@@ -23,6 +23,8 @@
     #include <arm_neon.h>
     #define vall_f32(x) vld1q_dup_f32(&(x))
     #define vall_f64(x) vld1q_dup_f64(&(x))
+    #define vmuladd_f32(a, b, c) vfmaq_f32(c, a, b)
+    #define vmuladd_f64(a, b, c) vfmaq_f64(c, a, b)
 #endif
 
 #define RED(string) "\x1b[31m" string "\x1b[0m"
--- src/recurrence/recurrence_NEON.c
+++ src/recurrence/recurrence_NEON.c
@@ -2,22 +2,22 @@
 
 #ifdef __aarch64__
     void horner_NEON(const int n, const double * c, const int incc, const int m, double * x, double * f) {
-        HORNER_KERNEL(double, float64x2_t, 2, 16, vld1q_f64, vst1q_f64, vmuladd, vall_f64)
+        HORNER_KERNEL(double, float64x2_t, 2, 16, vld1q_f64, vst1q_f64, vmuladd_f64, vall_f64)
     }
     void horner_NEONf(const int n, const float * c, const int incc, const int m, float * x, float * f) {
-        HORNER_KERNEL(float, float32x4_t, 4, 16, vld1q_f32, vst1q_f32, vmuladd, vall_f32)
+        HORNER_KERNEL(float, float32x4_t, 4, 16, vld1q_f32, vst1q_f32, vmuladd_f32, vall_f32)
     }
     void clenshaw_NEON(const int n, const double * c, const int incc, const int m, double * x, double * f) {
-        CLENSHAW_KERNEL(double, float64x2_t, 2, 8, vld1q_f64, vst1q_f64, vmuladd, vall_f64)
+        CLENSHAW_KERNEL(double, float64x2_t, 2, 8, vld1q_f64, vst1q_f64, vmuladd_f64, vall_f64)
     }
     void clenshaw_NEONf(const int n, const float * c, const int incc, const int m, float * x, float * f) {
-        CLENSHAW_KERNEL(float, float32x4_t, 4, 8, vld1q_f32, vst1q_f32, vmuladd, vall_f32)
+        CLENSHAW_KERNEL(float, float32x4_t, 4, 8, vld1q_f32, vst1q_f32, vmuladd_f32, vall_f32)
     }
     void orthogonal_polynomial_clenshaw_NEON(const int n, const double * c, const int incc, const double * A, const double * B, const double * C, const int m, double * x, double * phi0, double * f) {
-        ORTHOGONAL_POLYNOMIAL_CLENSHAW_KERNEL(double, float64x2_t, 2, 8, vld1q_f64, vst1q_f64, vmuladd, vmulsub, vall_f64)
+        ORTHOGONAL_POLYNOMIAL_CLENSHAW_KERNEL(double, float64x2_t, 2, 8, vld1q_f64, vst1q_f64, vmuladd_f64, vmulsub, vall_f64)
     }
     void orthogonal_polynomial_clenshaw_NEONf(const int n, const float * c, const int incc, const float * A, const float * B, const float * C, const int m, float * x, float * phi0, float * f) {
-        ORTHOGONAL_POLYNOMIAL_CLENSHAW_KERNEL(float, float32x4_t, 2, 8, vld1q_f32, vst1q_f32, vmuladd, vmulsub, vall_f32)
+        ORTHOGONAL_POLYNOMIAL_CLENSHAW_KERNEL(float, float32x4_t, 2, 8, vld1q_f32, vst1q_f32, vmuladd_f32, vmulsub, vall_f32)
     }
 #else
     void horner_NEON(const int n, const double * c, const int incc, const int m, double * x, double * f) {horner_default(n, c, incc, m, x, f);}
